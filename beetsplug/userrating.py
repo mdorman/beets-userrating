4b96898d06e00ba585d10a8a2704f14f36affac8
Michael Alan Dorman <mdorman@ironicdesign.com>
2017-11-09 17:55:27 -0500

    Basic structure of the plugin

---
+++ beetsplug/userrating.py
@@ -0,0 +1,45 @@
+# Copyright 2017, Michael Dorman.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+from beets import mediafile
+from beets import plugins
+from beets import ui
+from beets.dbcore import types
+
+class UserRatingsPlugin(plugins.BeetsPlugin):
+    """
+    A plugin for managing track ratings.
+
+    We're using the POPM tag http://id3.org/id3v2.3.0#Popularimeter as
+    our storage format (values 0-255, with 0 meaning no rating).
+    """
+
+    item_types = {
+        'userrating': types.INTEGER
+    }
+
+    def __init__(self):
+        super(UserRatingsPlugin, self).__init__()
+
+        self.config.add({
+            # Should we automatically import any values we find?
+            'auto': True,
+            # Should we overwrite an existing entry?
+            'overwrite': False,
+        })
+
+        userrating_field = mediafile.MediaField(
+            out_type=int
+        )
+
+        self.add_media_field('userrating', userrating_field)
