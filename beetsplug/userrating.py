31f2ec080a637d667b46e5e4416611847f2ee7ef
Michael Alan Dorman <mdorman@ironicdesign.com>
2017-11-28 07:57:40 -0500

    Add back some import stuff?

--- beetsplug/userrating.py
+++ beetsplug/userrating.py
@@ -38,6 +38,10 @@ class UserRatingsPlugin(plugins.BeetsPlugin):
             'overwrite': False,
         })
 
+        # Add importing ratings to the import process
+        if self.config['auto']:
+            self.import_stages = [self.imported]
+
         # Given the complexity of the storage style implementations, I
         # find it handy to allow them to do unified logging.
         userrating_field = mediafile.MediaField(
@@ -47,6 +51,58 @@ class UserRatingsPlugin(plugins.BeetsPlugin):
         )
 
         self.add_media_field('userrating', userrating_field)
+
+    # We do present a command, though it doesn't do anything as yet
+    def commands(self):
+        """
+        Return the "userrating" ui subcommand.
+        """
+
+        cmd = ui.Subcommand('userrating', help=u'manage user ratings for tracks')
+        cmd.func = lambda lib, opts, args: self.handle_tracks(lib.items(ui.decargs(args)))
+        return [cmd]
+
+    def imported(self, session, task):
+        """
+        Add rating info to items of ``task`` during import.
+        """
+
+        self.handle_tracks(task.imported_items())
+
+    def handle_tracks(self, items):
+        """
+        Abstract out our iteration code.
+        """
+
+        for item in items:
+            self.handle_track(item)
+
+    def handle_track(self, item):
+        """
+        Ask for user rating for track and store it in the item.
+
+        If user rating information is already present in the item,
+        nothing is done unless ``overwrite`` has been set.
+        """
+
+        if (item['userrating'] != 0) and (not self.config['overwrite'].get(bool)):
+            self._log.debug(u'Skipping already-rated track {0}', item)
+            return
+
+        self._log.debug(u'Getting rating for {0}', item)
+
+        # Get any rating already in the file
+        rating = mediafile.MediaFile(item.path).userrating
+
+        self._log.debug(u'Found rating {0}', rating)
+
+        if rating:
+            item['userrating'] = rating;
+            item.store()
+            self._log.debug(u'Applied rating {0}', item['userrating'])
+        else:
+            # We should consider asking here
+            self._log.debug(u'No rating found')
 
 class MP3UserRatingStorageStyle(mediafile.MP3StorageStyle):
     """
